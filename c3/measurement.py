8ceee055ac2bc040fb1b3c7ea5fc616bd611f679
Nicolas Wittler <n.wittler@pm.me>
2022-03-28 11:26:27 +0200

    Analytic models

--- c3/measurement.py
+++ c3/measurement.py
@@ -1,5 +1,8 @@
+import tensorflow as tf
+
 from typing import List
 
+from c3.c3objs import Quantity
 from c3.parametermap import ParameterMap
 from c3.propagation import PWC
 from c3.signal.gates import Instruction
@@ -25,3 +28,26 @@ class UnitaryInfid:
             ideal = instr.get_ideal_gate(self.dims)
             infid += 1 - tf_unitary_overlap(actual, ideal)
         return infid / len(instructions)
+
+
+class RabiExperiment:
+    def __init__(self, qubit_freq) -> None:
+        self.qubit_freq: Quantity = qubit_freq
+        self.pmap = ParameterMap()
+        self.pmap._pars = {
+            "amp": Quantity(100e6, unit="Hz 2pi"),
+            "freq": Quantity(5e9, unit="Hz 2pi"),
+            "time": Quantity(10e-9, unit="s"),
+        }
+        self.pmap.opt_map = [[("amp")], [("freq")], [("time")]]
+
+    def measure(self, parameters):
+        self.pmap.set_parameters_scaled(parameters)
+        amp = self.pmap.get_parameter(["amp"]).get_value()
+        freq = self.pmap.get_parameter(["freq"]).get_value()
+        t = self.pmap.get_parameter(["time"]).get_value()
+        q_freq = self.qubit_freq.get_value()
+        diff_sq = (q_freq - freq) ** 2
+        return tf.cos(tf.sqrt(diff_sq + amp**2) / 2 * t) / tf.sqrt(
+            1 + diff_sq / (amp**2)
+        )
