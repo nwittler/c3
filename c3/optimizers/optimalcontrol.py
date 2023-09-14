cafdcd37497172bea26fe136749b4e4632cebb97
Nicolas Wittler <n.wittler@pm.me>
2023-09-14 16:22:16 +0200

    Callback handling

--- c3/optimizers/optimalcontrol.py
+++ c3/optimizers/optimalcontrol.py
@@ -103,12 +103,15 @@ class OptimalControl(Optimizer):
         else:
             cb_fids = callback_fids
         for cb_fid in cb_fids:
-            try:
-                cb_fid_func = fidelities[cb_fid]
-            except KeyError:
-                raise Exception(f"C3:ERROR:Unkown goal function: {cb_fid}")
-            print(f"C3:STATUS:Found {cb_fid} in libraries.")
-            self.callback_fids.append(cb_fid_func)
+            if isinstance(callback_fids, str):
+                try:
+                    cb_fid_func = fidelities[cb_fid]
+                except KeyError:
+                    raise Exception(f"C3:ERROR:Unkown goal function: {cb_fid}")
+                print(f"C3:STATUS:Found {cb_fid} in libraries.")
+                self.callback_fids.append(cb_fid_func)
+            else:
+                self.callback_fids.append(cb_fid)
 
     def log_setup(self) -> None:
         """
